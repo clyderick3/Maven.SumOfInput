9039d75972d06926476c490e21874079d688c3b5
Clyde Broderick <clyderick3@gmail.com>
2022-02-26 11:57:28 -0500

    Added most code. Corrected punctuations.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,12 @@
 public class Main {
 
     public static void main(String[] args){
+int n = 100;
+int sum = 0;
 
+for (int i = 0; i<=n; i++) {
+    sum = sum + i;
+}
+System.out.println(sum);
     }
 }
