f49ac925b1b237151d9c00e006502a20beb15818
Clyde Broderick <clyderick3@gmail.com>
2022-02-26 13:00:12 -0500

    Fixed for input.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,22 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-int n = 100;
+//int n = 100;
+int x = 0;
 int sum = 0;
+Scanner requestNumber = new Scanner(System.in);
+System.out.println("Input your number.");
+x = requestNumber.nextInt();
 
-for (int i = 0; i<=n; i++) {
+for (int i = 0; i<=x; i++) {
     sum = sum + i;
 }
-System.out.println(sum);
+
+System.out.println("The sum of this input nunmber is " + sum);
     }
 }
